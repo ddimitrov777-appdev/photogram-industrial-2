9f757edc32f4140979b5bb6028a46d072a4e5abf
ddimitrov777 <ddimitro@chicagobooth.edu>
2021-04-20 21:52:34 +0000

    created it

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root "photos#index"
   resources :comments
   resources :follow_requests
+  resources :likes
   resources :photos
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
