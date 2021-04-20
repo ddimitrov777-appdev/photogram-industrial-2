098aa412ed49570b811f869b4040a3c774443d0b
ddimitrov777 <ddimitro@chicagobooth.edu>
2021-04-20 23:23:17 +0000

    newstuff

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
-  belongs_to :author
-  belongs_to :photo
+  
+  belongs_to :author, class_name: "User", counter_cache: true
+  belongs_to :photo, counter_cache: true
+
+  validates :body, presence: true
 end
