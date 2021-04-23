1c309eea2bd0b3b54d7c8b3c96a560580393dea8
ddimitrov777 <ddimitro@chicagobooth.edu>
2021-04-23 01:57:11 +0000

    other tabs

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,13 @@ Rails.application.routes.draw do
   resources :photos
   resources :users, show: :only
   
-   get "/:username" => "users#show"
+  
+  get ":username" => "users#show", as: :user
+  get ":username/liked" => "users#liked", as: :liked
+  get ":username/feed" => "users#feed", as: :feed
+  get ":username/discover" => "users#discover", as: :discover
+  get ":username/followers" => "users#followers", as: :followers
+  get ":username/following" => "users#following", as: :following
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
