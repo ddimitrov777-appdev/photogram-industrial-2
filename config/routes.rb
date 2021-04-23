6633a5c713a930725975b2ccba010d6c720821cd
ddimitrov777 <ddimitro@chicagobooth.edu>
2021-04-23 01:35:07 +0000

    it works

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,7 @@ Rails.application.routes.draw do
 
   #get “/users/:id”  => "users#show", as: :user
 
-  
-
+ 
   devise_for :users
 
   resources :comments
@@ -14,5 +13,7 @@ Rails.application.routes.draw do
   resources :photos
   resources :users, show: :only
   
+   get "/:username" => "users#show"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
